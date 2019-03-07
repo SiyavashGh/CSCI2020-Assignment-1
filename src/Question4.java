91fe2c80fada61ef617c414ac5ec1c0671f31118
UnfoldingCode <46750873+UnfoldingCode@users.noreply.github.com>
2019-03-06 23:25:34 -0500

    Add files via upload

--- src/Question4.java
+++ src/Question4.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.lang.*;
-import javafx.scene.control.Alert;
 import javafx.scene.layout.GridPane;
 import javafx.geometry.Insets;
 import java.io.IOException;
@@ -19,49 +18,36 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-public class Question4 extends Application {
+public class Part4 extends Application {
     //public static int [] arr=new int[26];
     private String fileName;
     private TextField nameInput = new TextField(fileName);
     private Stage primaryStage;
     private File file;
-    private Alert alert = new Alert(Alert.AlertType.INFORMATION);
-
     @Override
     public void start(Stage primaryStage) throws IOException {
-        alert.setHeaderText("Choose a text file");
-        alert.setContentText("This graph will need a text file");
-        alert.showAndWait();
-
-        while(file==null){
-            chooseFile();
-        }
+        chooseFile(); //function allows user to choose file to open
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(10,10,10,10));
-        grid.setVgap(8);
-        grid.setHgap(10);
-
+        grid.setVgap(8); //Set vertical gap for grid
+        grid.setHgap(10); //Set Horizontal gap for grid
         HBox paths = new HBox();
-
-        Label namef = new Label("Name of file");
+        Label namef = new Label("Choose file Path");
         nameInput.setMinWidth(300);
-        Button view = new Button("View");
-
+        Button view = new Button("Pick File");
         paths.getChildren().addAll(namef, nameInput, view);
-
-        view.setOnAction(event -> {
+        view.setOnAction(event -> { //When button is clicked, calls open file function
             chooseFile();
         });
-
+        nameInput.setOnAction(event->{ //When enter is clicked, calls open File function
+            chooseFile();
+                });
         grid.addRow(10, paths);
-
-        char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-        int count = 0;
-        String contents = new String(Files.readAllBytes(Paths.get(file.toURI())));
-        contents = contents.toUpperCase();
-        HashMap<Character, Integer> charCount = new HashMap<>();
-        char[] strArray = contents.toCharArray();
+        char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray(); //Character Array of alphabet
+        String contents = new String(Files.readAllBytes(Paths.get(file.toURI()))); //Creates String from file
+        contents = contents.toUpperCase(); //Changes String to all uppercase characters
+        HashMap<Character, Integer> charCount = new HashMap<>(); //Create Hashmap for characters/count of each char
+        char[] strArray = contents.toCharArray(); //Creates array of elemts from file string
         for (char c : strArray) {
             if (charCount.containsKey(c)) {
                 charCount.put(c, charCount.get(c) + 1);
@@ -71,43 +57,31 @@ public class Question4 extends Application {
         }
         final CategoryAxis xAxis = new CategoryAxis();
         final NumberAxis yAxis = new NumberAxis();
-        final BarChart<String, Number> bc = new BarChart<>(xAxis, yAxis);
+        final BarChart<String, Number> bc = new BarChart<>(xAxis, yAxis); //Creates barchart
         bc.setTitle("Alphabet Occurences");
         xAxis.setLabel("Letter");
         yAxis.setLabel("Value");
         XYChart.Series series1 = new XYChart.Series();
-        series1.setName("First File");
-        for(char l : alphabet)
+        series1.setName("File");
+        for(char l : alphabet) //adds XY values to bar chart
             if (charCount.containsKey(l)) {
                 series1.getData().add(new XYChart.Data(Character.toString(l), charCount.get(l)));
             } else {
                 series1.getData().add(new XYChart.Data(Character.toString(l), 0));
             }
         Scene scene = new Scene(grid);
+        bc.getData().addAll(series1);
         grid.getChildren().addAll(bc);
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
-
-
     private void chooseFile(){
         FileChooser filechooser = new FileChooser();
         filechooser.setTitle("Choose a text file");
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
-                "TXT files (*.txt)", "*.txt");
-        filechooser.getExtensionFilters().add(extFilter);
         file = filechooser.showOpenDialog(primaryStage);
-        if (file == null) {
-            alert.setContentText("No file was selected");
-            alert.setHeaderText("");
-            alert.showAndWait();
-        } else{
-            nameInput.setText(file.getAbsolutePath());
-            fileName = file.getName();
-        }
+        nameInput.setText(file.getAbsolutePath());
+        fileName = file.getName();
     }
-
     public static void main (String[]args){
         Application.launch(args);
     }
